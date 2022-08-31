86894ad745430d7127822dddbcb5fb01328b4d1e
noahbenson <nben@uw.edu>
2022-08-31 16:32:46 -0700

    cleanup

--- hcpannot/__init__.py
+++ hcpannot/__init__.py
@@ -12,5 +12,6 @@ from .core import (plot_angle, plot_eccen, plot_curv, plot_hmbound,
                    plot_isoecc, generate_images, label_to_segs)
 from .interface import (subject_ids, subject_data, ROITool,
                         imgrid_to_flatmap, flatmap_to_imgrid)
-from .analysis import (vc_plan, subject_list_1, subject_list_2, subject_list_3)
-
+from .analysis import (vc_plan, to_data_path, save_contours, load_contours,
+                       subject_list_1, subject_list_2, subject_list_3)
+from .io import (load_traces, load_paths)
