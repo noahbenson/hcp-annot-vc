97df3bc00e22e2f913d30dacc7c71d9cc9ff7bd6
BrendaQiu <siyueq@uw.edu>
2025-04-11 13:39:23 -0700

    remove manual hcpannot import

--- src/hcpannot/analysis/__init__.py
+++ src/hcpannot/analysis/__init__.py
@@ -4,3 +4,15 @@ Preprocessing code goes in the `hcpannot.proc` subpackage; code for analyzing
 the preprocessed data goes here.
 """
 
+
+from .contour import (
+    plot_rater_contours,
+    plot_lc
+)
+
+from .lmm import (
+    postprocess_result,
+    get_contour_data,
+    plot_lw,
+    plot_hmap
+)
